625b865ab81ea95b4f1733b62d0d94b114bf2c53
makise-homura <akemi_homura@kurisa.ch>
2017-04-13 03:24:00 +0300

    Regexp fixed; link to pleer.net gateway fixed.

--- lib/linktoplayer.php
+++ lib/linktoplayer.php
@@ -1,7 +1,7 @@
 <?php
 // This script is a gateway to pleer.net that allows to embed http pages to https.
 // It ensures that id contains only accepted characters.
-if (preg_match('^[A-Za-z0-9]+$', $_GET['id']) > 0)
+if (preg_match('/^[A-Za-z0-9]+$/', $_GET['id']) > 0)
 {
 	header("Location: http://embedpleer.net/normal/track?id=" . $_GET['id']);
 }
