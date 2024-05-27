25bd3b3d584313cf0f693cae068c080b0e87721c
makise-homura <akemi_homura@kurisa.ch>
2024-05-27 22:30:34 +0300

    Fix PHP error with setcookie_strict()

--- inc/func/misc.php
+++ inc/func/misc.php
@@ -236,7 +236,7 @@ function do_redirect($url, $ispost = false, $file = '') {
 	$headermethod = true;
 
 	if ($headermethod) {
-		setcookie_strict('tothread', $gtt, 0, '/', KU_DOMAIN);
+		/* setcookie_strict('tothread', $gtt, 0, '/', KU_DOMAIN); - Was this even working at all? */
 		if ($ispost) {
 			header('Location: ' . $url);
 		} else {
